de0347f82de8dd5d9094efa089cb7805479e09dd
NoodleSamaChan <lferraraccio@outlook.com>
2023-12-12 20:04:08 +0100

    completed d1 of AoC

--- 2018/01/01.py
+++ 2018/01/01.py
@@ -1,23 +1,15 @@
-with open('/home/noodlesama/AoC/2018/01/input', 'r') as file:
+with open('/Users/lunaferraraccio/Desktop/lille/advent_of_code/2018/01/input', 'r') as file:
     file_to_check = list(file)
     result = 0
-    list_of_results = []
-    new_list = []
+    list_of_results = set()
     duplicates = []
 
     while duplicates == []:
         for i in range(len(file_to_check)):
-            if file_to_check[i][0] == '+':
-                result = result + int(file_to_check[i])
-                list_of_results.append(result)
-            elif file_to_check[i][0] == '-':
-                result = result + int(file_to_check[i])
-                list_of_results.append(result)
-        for number in list_of_results:
-            if number not in new_list:
-                new_list.append(number)
-            else:
-                duplicates.append(number)
+            result = result + int(file_to_check[i])
+            if result in list_of_results:
+                duplicates.append(result)
                 break
+            else:
+                list_of_results.add(result)       
     print(duplicates)
-
