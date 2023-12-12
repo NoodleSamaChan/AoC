409ac5a759969d8d20c69da564eb00a746d0d35b
Luna <lferraraccio@outlook.com>
2023-12-12 16:29:07 +0100

    changes made to d1 of AoC 2018

--- 2018/01/01.py
+++ 2018/01/01.py
@@ -2,22 +2,22 @@ with open('/home/noodlesama/AoC/2018/01/input', 'r') as file:
     file_to_check = list(file)
     result = 0
     list_of_results = []
-    for i in range(len(file_to_check)):
-        if file_to_check[i][0] == '+':
-            result = result + int(file_to_check[i])
-            list_of_results.append(result)
-        elif file_to_check[i][0] == '-':
-            result = result + int(file_to_check[i])
-            list_of_results.append(result)
-
-    example = [1, 4, 6, 8, 4, 5, 3]
     new_list = []
     duplicates = []
-    for number in list_of_results:
-        if number not in new_list:
-            new_list.append(number)
-        else:
-            duplicates.append(number)
+
+    while duplicates == []:
+        for i in range(len(file_to_check)):
+            if file_to_check[i][0] == '+':
+                result = result + int(file_to_check[i])
+                list_of_results.append(result)
+            elif file_to_check[i][0] == '-':
+                result = result + int(file_to_check[i])
+                list_of_results.append(result)
+        for number in list_of_results:
+            if number not in new_list:
+                new_list.append(number)
+            else:
+                duplicates.append(number)
+                break
     print(duplicates)
-    print(new_list)
 
