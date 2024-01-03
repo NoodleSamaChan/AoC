34a53f7ffeea01651345010ffa77d1e2bac48a02
NoodleSamaChan <lferraraccio@outlook.com>
2024-01-03 20:51:13 +0100

    day 1 AOC 2022 in rust

---
+++ 2022/01/rust_02/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+fn main() {
+    let file_contents = fs::read_to_string("/Users/lunaferraraccio/Desktop/lille/advent_of_code/2022/01/rust_02/src/input_test")
+    .expect("LogRocket: Should have been able to read the file");
+
+    let mut biggest_number: i32 = 0;
+    let mut biggest_number_two: i32 = 0;
+    let mut biggest_number_three: i32 = 0;
+    let mut calories_per_elf: i32 = 0;
+    let mut vec: Vec<i32> = Vec::new();
+
+
+    for first in file_contents.lines() {
+        if first != "" {
+            let first: i32 = first.parse().unwrap();
+            calories_per_elf = calories_per_elf + first;
+            
+
+        } else {
+            vec.push(calories_per_elf);
+            calories_per_elf = 0
+        }
+    }
+    println!("{:#?}", vec);
+//    biggest_number = vec.iter().max();
+//    vec.retain(|value| *value != biggest_number);
+//    biggest_number_two = vec.iter().max();
+//    vec.retain(|value| *value != biggest_number);
+//    biggest_number_three = vec.iter().max();
+
+//    println!("{}", (biggest_number + biggest_number_two + biggest_number_three))
+}
